f073a915407d047828fe22d4450a2991f83e678f
Pravin <pravin.puducherry@gmail.com>
2026-02-14 13:13:27 +0530

    Re-organizing sql folder for jenkins implementation

--- sql/02_directory_creation.sql
+++ sql/02_directory_creation.sql
@@ -4,8 +4,3 @@
 CREATE OR REPLACE DIRECTORY source_data_dir AS '/data';
 -- Verify access (This checks if Oracle can "see" the folder)
 SELECT * FROM all_directories WHERE directory_name = 'SOURCE_DATA_DIR';
-
-
-
--- Setting read write permission to our user, run as SYSDBA
-GRANT READ, WRITE ON DIRECTORY SOURCE_DATA_DIR TO RETAIL_DW;
---
+++ sql/03_grant_to_dir_sys.sql
@@ -0,0 +1,2 @@
+-- Setting read write permission to our user, run as SYSDBA
+GRANT READ, WRITE ON DIRECTORY SOURCE_DATA_DIR TO RETAIL_DW;
