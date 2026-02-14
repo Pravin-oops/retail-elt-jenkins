b862616bf7c903dee6a3d209c426fd9403acf03a
Pravin <pravin.puducherry@gmail.com>
2026-02-14 14:18:59 +0530

    modified to run it in pipeline

--- sql/02_directory_creation.sql
+++ sql/02_directory_creation.sql
@@ -1,6 +1,8 @@
 -- Run this as RETAIL_DW
 -- Create a logical reference to the physical folder inside the Docker container
 -- Note: /data is the path we defined in docker-compose.yml
-CREATE OR REPLACE DIRECTORY source_data_dir AS '/data';
+CREATE OR REPLACE DIRECTORY source_data_dir AS '/data'
+/
 -- Verify access (This checks if Oracle can "see" the folder)
-SELECT * FROM all_directories WHERE directory_name = 'SOURCE_DATA_DIR';
+SELECT * FROM all_directories WHERE directory_name = 'SOURCE_DATA_DIR'
+/
